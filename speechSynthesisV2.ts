893ea2e574f8db8a869f22bcded3a7981241185d
zhang-tang <80501060+zhang-tang@users.noreply.github.com>
2023-06-08 09:52:19 +0800

    更新 speechSynthesisV2.ts

--- speechSynthesisV2.ts
+++ speechSynthesisV2.ts
@@ -1,13 +1,4 @@
-class SubMess {
-    ischar: number;
-    index: number;
-    length: number;
-    constructor(ischar: number, index: number, length: number) {
-        this.ischar = ischar;
-        this.index = index;
-        this.length = length;
-    }
-}
+
 
 
 //% weight=100 color=#9999FF icon="\uf0a1" block="Speech Synthesis (DFR0760)"
@@ -68,6 +59,17 @@ namespace speechSynthesisV2 {
     const PAUSE_SYNTHESIS = 0x03;
     const RECOVER_SYNTHESIS = 0x04;
 
+
+    class SubMess {
+        ischar: number;
+        index: number;
+        length: number;
+        constructor(ischar: number, index: number, length: number) {
+            this.ischar = ischar;
+            this.index = index;
+            this.length = length;
+        }
+    }
 
     let wordLenght = 0;
     let listIndex = 0;
